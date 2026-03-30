4c650a1e9b815040ce215a1411169f15f6741531
zaniyah-w <wardzr@.gcofc.edm>
2026-03-29 21:19:43 -0400

    updated

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        
+        <Stack.Screen name = "(tabs)"
+          options={{headerShown: false}} />
+
+          <Stack.Screen name = "index"
+            options={{ title: "Index"}} />
+
+      </Stack>
+
+    </SafeAreaProvider>
+  );
+}
