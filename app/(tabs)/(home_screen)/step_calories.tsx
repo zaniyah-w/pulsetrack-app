cc581f268c066dac251ffce95b641f6324fc090b
bricezm <bricezm@g.cofc.edu>
2026-04-04 16:08:03 -0400

    Removed unused file.

--- app/(tabs)/(home_screen)/step_calories.tsx
+++
@@ -1,7 +0,0 @@
-import React, {useState} from "react";
-import {View, StyleSheet} from "react-native";
-
-import getStepsForday from "./index";
-import MapView, {Marker, Polyline, MapPressEvent} from "react-native-maps";
-
-
